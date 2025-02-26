8daaa633f0fa157f8e8849a1f8cc1dbc75170684
vvitalsync <vvitalsync@gmail.com>
2025-02-26 20:49:57 +0000

    sixth-commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.vitalsync.app',
   appName: ' VitalSync',
-  webDir: 'dist'
+  webDir: 'dist/vital-sync/browser'
 };
 
 export default config;
